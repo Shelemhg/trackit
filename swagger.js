20eb906ef7557e561b7b7b390dbef930477499bf
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-03-11 11:52:22 -0600

    Changed swagger.js schemes from "http" to "https"

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: 'trackit-km9j.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';

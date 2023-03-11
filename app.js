ac3af57916de7d1717a65f6f33e37f14a274c5d1
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-03-11 11:46:31 -0600

    Added listen to production port to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {    
     console.log('listening on port ', port);

34c90809390078fc2622bef904a5c84f2e37a1f3
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-03-11 11:38:46 -0600

    First comming

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+const port = 3000;
+
+app.listen(port, () => {    
+    console.log('listening on port ', port);
+});
+
+app.get('/', (req, res) => {
+    res.send('Whats up');
+});
+
+
+// Swagger route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));

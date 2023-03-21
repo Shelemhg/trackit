8aed9bdeb9a428a913a4682bcb25e328d20a9e8c
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-03-21 17:30:37 -0600

    Added routes for login and logout with Okta Auth0

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send('Whats up');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { auth } = require('express-openid-connect');
+
 
 //Added Swagger Routes
 router.use('/', require('./swagger'));
@@ -9,6 +11,28 @@ router.use('/', require('./swagger'));
 
 //  Added Amazon Routes
 router.use('/amazon', require('./amazon'));
+
+
+
+
+// Oauth config and routes
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: 'http://localhost:3000',
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 
 
 

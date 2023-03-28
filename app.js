f0b04a4abe36ac07ae023a2f42ceb2a4efa168ac
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-03-28 12:12:09 -0600

    Updated the main route

--- app.js
+++ app.js
@@ -3,11 +3,37 @@ const app = express();
 const mongodb = require('./config/db.config.js');
 const bodyParser = require('body-parser');
 
+const { auth } = require('express-openid-connect');
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res, next) => {
-    res.send('Whats up');
+// app.get('/', (req, res, next) => {
+//     res.send('Whats up');
+// });
+
+
+
+
+// Auth0 config and routes
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: 'https://trackit-km9j.onrender.com',
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+
+
+
 
 
 app

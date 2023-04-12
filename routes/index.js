4695cb441e449aecfe6708d14b6dfbcc68a5ff33
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-04-11 20:55:58 -0600

    Updated comments

--- routes/index.js
+++ routes/index.js
@@ -19,26 +19,6 @@ router.use('/alibaba', require('./alibaba'));
 //  Aliexpress Route
 router.use('/aliexpress', require('./aliexpress'));
 
-// Auth0 config and routes
-// const config = {
-//     authRequired: false,
-//     auth0Logout: true,
-//     secret: process.env.SECRET,
-//     baseURL: 'https://trackit-km9j.onrender.com',
-//     clientID: process.env.CLIENT_ID,
-//     issuerBaseURL: process.env.ISSUER_BASE_URL
-// };
-
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-// router.use(auth(config));
-
-// // req.isAuthenticated is provided from the auth router
-// router.get('/', (req, res) => {
-//     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
-
-
-
 
 
 module.exports = router;

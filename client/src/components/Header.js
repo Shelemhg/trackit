a41362ad318f8cd3ef928da10ab26cd975023163
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2023-09-29 23:20:19 -0600

    Added images and format to home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,18 +4,20 @@ import logo from '../assets/images/logo2.png';
 export default function Header(){
 	return (
 		<nav className="nav-bar">
-			<img src={logo} alt="logo"></img>
+			<a href="/">
+            <img src={logo} alt="logo"></img>
+            </a>
 
             {/* Another way to add an external element is to require it */}
             {/* <img src={require('../assets/images/logo2.png')} alt="logo"></img> */}
-			<ul>
-				<li>
-					<a href="/">Home</a>
-				</li>
-				<li>
-					<a href="/login">Login</a>
-				</li>
-			</ul>
+			
+            <ul class="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
 		</nav>
 	)
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,48 @@
+import heroImg from '../assets/images/hero-img.png';
+import featureImg1 from '../assets/images/feature-1.png';
+import featureImg2 from '../assets/images/feature-2.png';
+import featureImg3 from '../assets/images/feature-3.png';
+import logo from '../assets/images/logo2.png';
+
+
 export default function Home () {
     return (
         <>
-            <h1>Home</h1>
+            <div class="container">
+                <section class="hero">
+                    <div class="hero-text">
+                        <h1>Your Amazon Price Tracker</h1>
+                        <p>Save money and stay updated on the products you love.</p>
+                        <a href="#" class="cta-button">Get Started</a>
+                    </div>
+                    <div class="hero-image">
+                        <img src={heroImg} alt="logo"></img>
+                    </div>
+                </section>
+
+                <section class="features">
+                    <div class="features-heading">
+                        <h2>Key Features</h2>
+                    </div>
+                    <div class="feature-container">
+                        <div class="feature">
+                            <img src={featureImg1} alt="logo"></img>
+                            <h3>Real-Time Tracking</h3>
+                            <p>Monitor price changes in real-time for your favorite Amazon products.</p>
+                        </div>
+                        <div class="feature">
+                            <img src={featureImg2} alt="logo"></img>
+                            <h3>Price Alerts</h3>
+                            <p>Receive price drop notifications via email or mobile alerts.</p>
+                        </div>
+                        <div class="feature">
+                            <img src={featureImg3} alt="logo"></img>
+                            <h3>Personalized Watchlist</h3>
+                            <p>Create a personalized watchlist of products to track your desired items.</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
